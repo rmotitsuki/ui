94b91f5fbdc1e5a56847e51875078aae90dda86d
rmotitsuki <rogerio.motitsuki@gmail.com>
2023-11-29 16:17:44 -0300

    Add preprod webpack build for UI debug

---
+++ webpack/webpack.preprod.js
@@ -0,0 +1,24 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+var webpack = require('webpack')
+
+module.exports = merge(
+  common,{
+    mode: 'development',
+    devtool: 'inline-source-map',
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"development"'
+        }
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: false,
+        debug: true
+      }),
+      new CleanWebpackPlugin(),
+    ]
+  });
