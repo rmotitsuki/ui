ce909d725e17dcfe088c0a1bd68bc9d7681c8bb3
Heriberto Luna <hluna006@fiu.edu>
2024-11-25 15:50:08 -0500

    Updated vue3-sfc-loader config/options

--- src/components/kytos/napp/Toolbar.vue
+++ src/components/kytos/napp/Toolbar.vue
@@ -10,7 +10,6 @@
  
  <script>
  import Vue from 'vue'
- import httpVueLoader from "./httpVueLoader.js"
  import { loadModule } from "vue3-sfc-loader"
  
  const options = {
@@ -18,16 +17,42 @@
    moduleCache: {
      vue: Vue
    },
+
+   pathResolve({ refPath, relPath }, options) {
+
+    if ( relPath === '.' )
+      return refPath;
+
+    if ( relPath[0] !== '.' && relPath[0] !== '/' )
+      return relPath;
+
+    return String(new URL(relPath, refPath === undefined ? window.location : refPath));
+   },
+
+   handleModule: async function (type, getContentData, path, options) {
+
+    switch (type) {
+      case '.css':
+        options.addStyle(await getContentData(false));
+        return null;
+      case '.kytos':
+        console.log("Kytos detected");
+        return null;
+      default: return undefined; // let vue3-sfc-loader handle this
+    }
+   },
+
+   async getFile(url) {
  
-   getFile(url) {
- 
-     return fetch(url).then(response => response.ok ? response.text() : Promise.reject(response));
+     const res = await fetch(url);
+     if ( !res.ok )
+        throw Object.assign(new Error(url+' '+res.statusText), { res });
+     return await res.text();
    },
  
-   addStyle(styleStr) {
+   addStyle(textContent) {
  
-     const style = document.createElement('style');
-     style.textContent = styleStr;
+     const style = Object.assign(document.createElement('style'), { textContent });
      const ref = document.head.getElementsByTagName('style')[0] || null;
      document.head.insertBefore(style, ref);
    }
