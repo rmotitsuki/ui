94bff228c1d1d6b16dc9832b34fe1489567d9e22
Heriberto Luna <hluna006@fiu.edu>
2025-02-25 12:52:57 -0500

    Added prod and preprod to config

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,40 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
-
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: './web-ui/',
-    assetsDir: './dist/',
-    sourcemap: true
-  },
-  test: {
-    environment: "happy-dom",
-    coverage: {
-      reporter: ['text', 'html'],
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (mode === 'preprod') {
+    return {
+      plugins: [vue()],
+      build: {
+        outDir: './web-ui/',
+        assetsDir: './dist/',
+        sourcemap: true,
+        minify: false
+      },
+      test: {
+        environment: "happy-dom",
+        coverage: {
+          reporter: ['text', 'html'],
+        }
+      },
+      css: {
+        devSourcemap: true,
+      }
+    }
+  } else {
+    return {
+      plugins: [vue()],
+      build: {
+        outDir: './web-ui/',
+        assetsDir: './dist/',
+        minify: true
+      },
+      test: {
+        environment: "happy-dom",
+        coverage: {
+          reporter: ['text', 'html'],
+        }
+      }
     }
   }
-});
+})
