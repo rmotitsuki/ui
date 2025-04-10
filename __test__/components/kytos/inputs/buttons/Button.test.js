3d9efaa51b0f7ecd1d6f3818d1cf103277cb394f
Heriberto Luna <hluna006@fiu.edu>
2025-04-10 15:49:28 -0400

    Added k-button unit tests

---
+++ __test__/components/kytos/inputs/buttons/Button.test.js
@@ -0,0 +1,96 @@
+import { mount, shallowMount } from '@vue/test-utils';
+import Button from '@/components/kytos/inputs/buttons/Button.vue';
+import { describe, test, expect, beforeAll, afterEach, vi } from "vitest";
+
+
+
+describe("Button.vue", () => {
+    let wrapper;
+    beforeAll(() => {
+        expect(Button).toBeTruthy();
+    });
+
+    afterEach(() => {
+        wrapper.unmount();
+        vi.restoreAllMocks();
+    });
+
+    //Inputs
+
+    describe("Props", () => {
+        test("Disabling Button", async () => {
+            wrapper = mount(Button);
+            expect(wrapper.exists()).toBe(true);
+            const mainInput = wrapper.get('[data-test="main-button"]');
+            expect(mainInput.element.hasAttribute('disabled')).toBe(false);
+
+            await wrapper.setProps({ isDisabled: true });
+
+            expect(mainInput.element.hasAttribute('disabled')).toBe(true);
+        });
+
+        test("Button Tooltip", async () => {
+            const testValue = 'test';
+            wrapper = mount(Button);
+            expect(wrapper.exists()).toBe(true);
+            const mainInput = wrapper.get('[data-test="main-button"]');
+
+            expect(mainInput.element.hasAttribute('title')).toBe(false);
+
+            await wrapper.setProps({ tooltip: testValue });
+
+            expect(mainInput.attributes('title')).toBe(testValue);
+        });
+    });
+
+    describe("User Interactions", () => {
+        test("Button Data/Write/Use", async () => {
+            wrapper = mount(Button);
+            expect(wrapper.exists()).toBe(true);
+            const mainInput = wrapper.get('[data-test="main-button"]');
+
+            await mainInput.trigger('click');
+
+            expect(wrapper.emitted('click')).toHaveLength(1);
+        });
+    });
+
+    //Outputs
+
+    describe("DOM Elements", () => {
+        test("Button", () => {
+            wrapper = mount(Button);
+            expect(wrapper.exists()).toBe(true);
+
+            expect(wrapper.find('[data-test="main-button"]').exists()).toBe(true);
+        });
+
+        test("Icon", async () => {
+            const testIcon = "arrow-right";
+            wrapper = shallowMount(Button);
+            expect(wrapper.exists()).toBe(true);
+
+            expect(wrapper.find('[data-test="main-icon"]').exists()).toBe(false);
+
+            await wrapper.setProps({ icon: testIcon });
+
+            expect(wrapper.find('[data-test="main-icon"]').exists()).toBe(true);
+
+            const icon = wrapper.get('[data-test="main-icon"]');
+
+            expect(icon.attributes('icon')).toBe(testIcon);
+        });
+    });
+
+    describe("Emits", () => {
+        test("Emit Button Click", async () => {
+            wrapper = mount(Button);
+            expect(wrapper.exists()).toBe(true);
+            const mainInput = wrapper.get('[data-test="main-button"]');
+
+            await mainInput.trigger('click');
+
+            expect(wrapper.emitted('click')).toHaveLength(1);
+        });
+    });
+});
