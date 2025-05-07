fbc9098093cd1001ddb0733a139d9193fd6f7c70
Heriberto Luna <hluna006@fiu.edu>
2025-05-07 10:57:50 -0400

    Added pinia test utils

--- __test__/components/kytos/napp/Toolbar.test.js
+++ __test__/components/kytos/napp/Toolbar.test.js
@@ -10,10 +10,10 @@ import {
   vi,
   beforeEach,
 } from "vitest";
-import { setActivePinia, createPinia } from "pinia";
+import { createTestingPinia } from "@pinia/testing";
 import { useToolbarStore } from "@/stores/toolbarStore.js";
 
-const mockComponentList = [
+const mockComponentList = {data: [
   {
     name: "kytos-maintenance-k-toolbar-main",
     url: "ui/kytos/maintenance/k-toolbar/main.kytos",
@@ -38,18 +38,15 @@ const mockComponentList = [
     name: "amlight-sdntrace_cp-k-toolbar-main",
     url: "ui/amlight/sdntrace_cp/k-toolbar/main.kytos",
   },
-];
+]};
 
-const spy = vi.spyOn(axios, "get").mockImplementation(() => mockComponentList);
+vi.spyOn(axios, "get").mockResolvedValue(mockComponentList);
+const mockFunction = vi.fn();
 
-describe("Input.vue", () => {
+describe("Toolbar.vue", () => {
   let wrapper;
   beforeAll(() => {
     expect(Toolbar).toBeTruthy();
-  });
-
-  beforeEach(() => {
-    setActivePinia(createPinia());
   });
 
   afterEach(() => {
@@ -60,7 +57,7 @@ describe("Input.vue", () => {
   //Inputs
 
   describe("Props", () => {
-    test("Disabling Input", async () => {
+    test("Active Toolbar Item", async () => {
       wrapper = mount(Input);
       expect(wrapper.exists()).toBe(true);
       const mainInput = wrapper.get('[data-test="main-input"]');
@@ -69,77 +66,6 @@ describe("Input.vue", () => {
       await wrapper.setProps({ isDisabled: true });
 
       expect(mainInput.element.hasAttribute("disabled")).toBe(true);
-    });
-
-    test("Default Input Value", async () => {
-      const testValue = "test";
-      wrapper = mount(Input);
-      expect(wrapper.exists()).toBe(true);
-      const mainInput = wrapper.get('[data-test="main-input"]');
-
-      expect(mainInput.element.hasAttribute("value")).toBe(true);
-
-      await wrapper.setProps({ value: testValue });
-
-      expect(mainInput.attributes("value")).toBe(testValue);
-    });
-
-    test("Input Tooltip", async () => {
-      const testValue = "test";
-      wrapper = mount(Input);
-      expect(wrapper.exists()).toBe(true);
-      const mainInput = wrapper.get('[data-test="main-input"]');
-
-      expect(mainInput.element.hasAttribute("title")).toBe(false);
-
-      await wrapper.setProps({ tooltip: testValue });
-
-      expect(mainInput.attributes("title")).toBe(testValue);
-    });
-
-    test("Input Placeholder", async () => {
-      const testValue = "test";
-      wrapper = mount(Input);
-      expect(wrapper.exists()).toBe(true);
-      const mainInput = wrapper.get('[data-test="main-input"]');
-
-      expect(mainInput.element.hasAttribute("placeholder")).toBe(false);
-
-      await wrapper.setProps({ placeholder: testValue });
-
-      expect(mainInput.attributes("placeholder")).toBe(testValue);
-    });
-
-    test("Input Action", async () => {
-      const fn = vi.fn();
-      const text = "test";
-      wrapper = mount(Input);
-      expect(wrapper.exists()).toBe(true);
-      const mainInput = wrapper.get('[data-test="main-input"]');
-
-      expect(wrapper.props().hasOwnProperty("action")).toBe(true);
-
-      await wrapper.setProps({ action: fn });
-
-      expect(wrapper.props("action")).toBe(fn);
-
-      await mainInput.setValue(text);
-
-      expect(fn).toHaveBeenCalledTimes(1);
-      expect(fn).toHaveBeenCalledWith(text);
-    });
-  });
-
-  describe("User Interactions", () => {
-    test("Input Data/Write/Use Input", async () => {
-      const text = "test";
-      wrapper = mount(Input);
-      expect(wrapper.exists()).toBe(true);
-      const mainInput = wrapper.get('[data-test="main-input"]');
-
-      await mainInput.setValue(text);
-
-      expect(mainInput.element.value).toContain(text);
     });
   });
 
@@ -150,22 +76,32 @@ describe("Input.vue", () => {
           active: 1,
         },
         global: {
+          plugins: [
+            createTestingPinia({
+              createSpy: vi.fn
+            }),
+          ],
           mocks: {
             $kytos_server: "http://localhost:8181/",
+            $http: axios,
+            $kytos: { component: mockFunction }
           },
         },
       });
       expect(wrapper.exists()).toBe(true);
+      const store = useToolbarStore();
 
-      expect(spy).toHaveBeenCalledTimes(1);
-      expect(spy).toHaveBeenCalledWith(
+      expect(axios.get).toHaveBeenCalledTimes(1);
+      expect(axios.get).toHaveBeenCalledWith(
         "http://localhost:8181/ui/k-toolbar"
       );
 
       await flushPromises();
 
-      
+      expect(store.toolbarItemsList).toEqual(mockComponentList.data);
+      expect(mockFunction).toHaveBeenCalledTimes(mockComponentList.data.length);
     });
+    //Fail
   });
 
   //Outputs
@@ -195,7 +131,7 @@ describe("Input.vue", () => {
     });
   });
 
-  describe("Emits", () => {
+  describe("vue3-sfc-loader", () => {
     test("Emit Input Value", async () => {
       const text = "test";
       wrapper = mount(Input);
@@ -209,20 +145,4 @@ describe("Input.vue", () => {
     });
   });
 
-  //V-Model
-
-  describe("V-Models", () => {
-    test("V-Model Value", async () => {
-      const text = "test";
-      wrapper = mount(Input, {
-        props: {
-          value: "initialText",
-          "onUpdate:value": (e) => wrapper.setProps({ value: e }),
-        },
-      });
-
-      await wrapper.get('[data-test="main-input"]').setValue(text);
-      expect(wrapper.props("value")).toBe(text);
-    });
-  });
 });
