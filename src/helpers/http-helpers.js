820bd99fae8fca23ded208fd6ff18a290779496a
Heriberto Luna <hluna006@fiu.edu>
2025-05-09 11:59:53 -0400

    Updated helper function

--- src/helpers/http-helpers.js
+++ src/helpers/http-helpers.js
@@ -1,22 +1,17 @@
-function format_error(str, error) {
-  //Regular expression matches strings with {s} or {d}
-  return str.replace(/{([sd])}/g, function (match) {
-    switch (match) {
-      case "{d}":
-        return error.response.data.description;
-      case "{s}":
-        return error.response.status;
-      default:
-        return match;
-    }
-  });
-}
-
-const post_error = (_this, error, title = "{s}", description = "{d}") => {
+const post_error = (
+  _this,
+  error,
+  title = "Status",
+  description = { prefix: "", suffix: "" }
+) => {
   let notification = {
     icon: "gear",
-    title: format_error(title, error),
-    description: format_error(description, error),
+    title: `${title} (${error.response.status}):`,
+    description: `${description.prefix}${
+      error.response.data.description
+        ? error.response.data.description
+        : error.response.data
+    }${description.suffix}`,
   };
 
   _this.$kytos.eventBus.$emit("setNotification", notification);
