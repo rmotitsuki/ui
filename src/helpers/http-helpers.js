ed5f64dee324b7b5ce7dd5ce77d2c67d01d7dfde
Blue <hluna006@fiu.edu>
2025-05-14 14:51:25 -0400

    Documented post_error function

--- src/helpers/http-helpers.js
+++ src/helpers/http-helpers.js
@@ -1,3 +1,14 @@
+  /**
+   * @param {Object} _this - The current Vue component instance.
+   * @param {Object} error - The error returned by Axios.
+   * @param {string} title - A title for the error notification.
+   * @param {Object} description - A description object for the body of the error notification. This contains:
+   * @param {string} description.prefix - The text that will go before the error description from the error object.
+   * @param {string} description.suffix - The text that will go after the error description from the error object.
+   * @returns {void}
+   * @description Emits an error notification that will be displayed in the terminal.
+   */
+
 const post_error = (
   _this,
   error,
